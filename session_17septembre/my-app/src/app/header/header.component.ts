fba90c0649836f2aa009b21c3d5a98b9224bb3c5
DidierDefrance <didier@d-defrance.fr>
2024-09-19 11:37:29 +0200

    s

--- session_17septembre/my-app/src/app/header/header.component.ts
+++ session_17septembre/my-app/src/app/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PreferencesService } from '../common/service/preferences.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
   @Input()
   titre = "titreParDefaut";
+
+  constructor(public preferencesService : PreferencesService) { 
+    console.log("dans constructeur de HeaderComponent , titre=" + this.titre);
+  }
+  ngOnInit(): void {
+    console.log("dans ngOnInit() de HeaderComponent , titre=" + this.titre)
+  }
 
 }
