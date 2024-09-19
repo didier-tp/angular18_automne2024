f03dca379eedec8d0300de2fe6aadd722bd0b162
DidierDefrance <didier@d-defrance.fr>
2024-09-19 11:57:59 +0200

    s

--- session_17septembre/my-app/src/app/common/service/preferences.service.ts
+++ session_17septembre/my-app/src/app/common/service/preferences.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { inject, PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreferencesService {
-  public couleurFondPreferee :string = 'lightgrey';
-  constructor() { }
+  private readonly platform = inject(PLATFORM_ID);
+
+  private _couleurFondPreferee: string;
+  public get couleurFondPreferee() {
+    return this._couleurFondPreferee;
+  }
+  public set couleurFondPreferee(c: string) {
+    this._couleurFondPreferee = c;
+    if (isPlatformBrowser(this.platform)) {
+      localStorage.setItem('preferences.couleurFond', c);
+    }
+  }
+  constructor() {
+    let c: string | null = 'lightgrey';
+    if (isPlatformBrowser(this.platform)) {
+      if (localStorage)
+        c = localStorage.getItem('preferences.couleurFond');
+    }
+    this._couleurFondPreferee = c ? c : 'lightgrey';
+  }
 }
