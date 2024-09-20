5c018fd78ee3a2a2223feffd9442e7cd50a56e13
DidierDefrance <didier@d-defrance.fr>
2024-09-20 11:47:32 +0200

    s

--- session_17septembre/my-app/src/app/app-routing.module.ts
+++ session_17septembre/my-app/src/app/app-routing.module.ts
@@ -6,11 +6,19 @@ import { BasicComponent } from './basic/basic.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { ConversionComponent } from './conversion/conversion.component';
 import { DeviseComponent } from './devise/devise.component';
+import { TvaComponent } from './basic/tva/tva.component';
+import { CalculatriceComponent } from './basic/calculatrice/calculatrice.component';
 
 const routes: Routes = [
   { path : "ngr-welcome" , component : WelcomeComponent},
   { path : "ngr-login" , component : LoginComponent} ,
-  { path : "ngr-basic" , component : BasicComponent},
+  { path : "ngr-basic" , component : BasicComponent ,
+    children: [
+      { path: 'tva', component: TvaComponent },
+      { path: 'calculatrice', component: CalculatriceComponent },
+      { path: '', redirectTo: 'tva', pathMatch: 'prefix'}
+      ]
+  },
   { path: 'ngr-conversion', component: ConversionComponent } ,
   { path : "ngr-reservation" , component : ReservationComponent},
   { path : "ngr-devise" , component : DeviseComponent},
