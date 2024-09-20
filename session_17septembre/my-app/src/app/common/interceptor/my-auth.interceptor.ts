801d170e8f090319bdecb8b65f2385dc523f66e4
DidierDefrance <didier@d-defrance.fr>
2024-09-20 10:40:23 +0200

    s

--- session_17septembre/my-app/src/app/common/interceptor/my-auth.interceptor.ts
+++ session_17septembre/my-app/src/app/common/interceptor/my-auth.interceptor.ts
@@ -5,16 +5,22 @@ export const myAuthInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req);
 };
 */
-
+import { inject, PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest }
   from '@angular/common/http';
 import { Observable } from 'rxjs';
 @Injectable()
 export class MyAuthInterceptor implements HttpInterceptor {
+  private readonly platform = inject(PLATFORM_ID);
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler)
     : Observable<HttpEvent<unknown>> {
-    const token = sessionStorage.getItem('access_token');
+      let token : string | null = "";
+      if (isPlatformBrowser(this.platform)) {
+           token = sessionStorage.getItem('access_token');
+      }
     const authReq = request.clone({
       headers: request.headers.set('Authorization', 'Bearer ' + token)
     });
