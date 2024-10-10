0aef4952747d63bcb13619f5729804b59c60e88c
didierDefrance <didier@d-defrance.fr>
2024-10-10 12:24:51 +0200

    s

--- session_9octobre/my-app/src/app/header/header.component.ts
+++ session_9octobre/my-app/src/app/header/header.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+   ngOnInit(): void {
+     console.log(" dans ngOnInit() ,  titre="+this.titre);
+   }
+
+   constructor(){
+    console.log(" dans constructor() ,  titre="+this.titre);
+   }
 
    @Input()
    titre = "titreParDefaut"
