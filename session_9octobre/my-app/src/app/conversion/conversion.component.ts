12a1b66adea1a99c42ba34d121b7fe713589aa99
didierDefrance <didier@d-defrance.fr>
2024-10-11 10:10:15 +0200

    s

--- session_9octobre/my-app/src/app/conversion/conversion.component.ts
+++ session_9octobre/my-app/src/app/conversion/conversion.component.ts
@@ -13,6 +13,7 @@ export class ConversionComponent implements OnInit {
   codeDeviseSource: string = "?";
   codeDeviseCible: string = "?";
   montantConverti: number = 0;
+  message = "";
   listeDevises: Devise[] = []; //à choisir dans liste déroulante.
   constructor(private _deviseService: DeviseService) { }
 
@@ -26,7 +27,7 @@ export class ConversionComponent implements OnInit {
           this.montantConverti = res;
           console.log("resultat obtenu en différé")
         },
-        error: (err) => { console.log(messageFromError(err,"echec conversion:")) }
+        error: (err) => { console.log(messageFromError(err, "echec conversion:")) }
       });
     console.log("suite immédiate (sans attente) de onConvertir");
     //Attention : sur cette ligne , le résultat n'est à ce stade pas encore connu
@@ -34,17 +35,17 @@ export class ConversionComponent implements OnInit {
   }
 
   async onConvertir() {
-    try{
-     this.montantConverti = await  firstValueFrom(
-         this._deviseService.convertir$(this.montant,
-        this.codeDeviseSource,
-        this.codeDeviseCible)
+    try {
+      this.montantConverti = await firstValueFrom(
+        this._deviseService.convertir$(this.montant,
+          this.codeDeviseSource,
+          this.codeDeviseCible)
       );
 
-    }catch(ex){
-      console.log(messageFromEx(ex,"echec conversion:"));
+    } catch (ex) {
+      console.log(messageFromEx(ex, "echec conversion:"));
     }
-    
+
   }
 
   initListeDevises(tabDevises: Devise[]) {
@@ -63,4 +64,29 @@ export class ConversionComponent implements OnInit {
         error: (err) => { console.log("error:" + err + JSON.stringify(err)) }
       });
   }
+
+  codeToUpdate = "?";
+  changeToUpdate = 1;
+  async onUpdate() {
+    try {
+      let d: Devise;
+      let deviseTemp: Devise | undefined;
+      for (d of this.listeDevises) {
+        if (d.code == this.codeToUpdate) {
+          deviseTemp = JSON.parse(JSON.stringify(d));
+        }
+      }
+      if (deviseTemp == null)
+        this.message = "pas de devise pour ce code";
+      else {
+        deviseTemp.change = this.changeToUpdate;
+        await firstValueFrom(this._deviseService.putDevise$(deviseTemp));
+        this.message = "mise à jour ok";
+      }
+    } catch (err) {
+      console.log(err);
+      this.message = <string>JSON.stringify(err);
+    }
+  }
+
 }
