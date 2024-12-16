91a956f45c6b865eeed759e8d92c747fded0286f
DidierDefrance <didier@d-defrance.fr>
2024-12-16 20:29:24 +0100

    s

---
+++ session16decembre/my-app/src/app/basic/basic.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic',
+  standalone: false,
+  
+  templateUrl: './basic.component.html',
+  styleUrl: './basic.component.scss'
+})
+export class BasicComponent {
+
+}
