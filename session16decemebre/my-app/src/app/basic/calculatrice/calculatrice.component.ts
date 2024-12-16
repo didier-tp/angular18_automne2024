c57879e56fbf1e265248474385fd97be95963777
DidierDefrance <didier@d-defrance.fr>
2024-12-16 15:44:31 +0100

    s

---
+++ session16decemebre/my-app/src/app/basic/calculatrice/calculatrice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculatrice',
+  standalone: false,
+  
+  templateUrl: './calculatrice.component.html',
+  styleUrl: './calculatrice.component.scss'
+})
+export class CalculatriceComponent {
+
+}
---
+++ session16decemebre/my-app/src/app/basic/tva/tva.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tva',
+  standalone: false,
+  
+  templateUrl: './tva.component.html',
+  styleUrl: './tva.component.scss'
+})
+export class TvaComponent {
+
+}
