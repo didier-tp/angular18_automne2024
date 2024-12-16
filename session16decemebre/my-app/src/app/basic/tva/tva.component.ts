977b748aec83d43f9e0f023900de160b84c0b3e0
DidierDefrance <didier@d-defrance.fr>
2024-12-16 16:48:54 +0100

    s

--- session16decemebre/my-app/src/app/basic/tva/tva.component.ts
+++ session16decemebre/my-app/src/app/basic/tva/tva.component.ts
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './tva.component.scss'
 })
 export class TvaComponent {
+//à saisir ou choisir:
+  ht /*: number*/ = 0;
+  tauxTva /* : number*/ = 20; //en %
+
+  listeTaux = [ 5, 10, 20]; //liste des taux possibles (en %)
+
+//à calculer et afficher:
+  tva /*:number*/ = 0;
+  ttc /*:number*/ = 0;
+
+onCalculerTvaEtTtc(){
+  this.tva = this.ht * this.tauxTva / 100;
+  this.ttc = Number(this.tva) + Number(this.ht);
+}
 
 }
