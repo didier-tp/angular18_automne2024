51fbab4824707eab4af4c66f90476c151d0d873d
DidierDefrance <didier@d-defrance.fr>
2024-12-19 16:31:04 +0100

    s

--- session16decembre/my-app/src/app/basic/tva/tva.component.spec.ts
+++ session16decembre/my-app/src/app/basic/tva/tva.component.spec.ts
@@ -35,6 +35,37 @@ describe('TvaComponent', () => {
     expect(spanTvaElt.innerText).toContain('40');
     });
 
+
+    it('tva(200,10)=20 from ihm', () => {
+      const compNativeElt = fixture.debugElement.nativeElement;
+      let htInputElt = compNativeElt.querySelector("input[name='ht']");
+      htInputElt.value=200;
+      htInputElt.dispatchEvent(new Event('input'));
+      
+      let tauxSelectElt = compNativeElt.querySelector("select[name='taux']");
+      let optionElt = null;
+      for(let opt of tauxSelectElt.children){
+        if(opt.innerText=="10%"){
+          optionElt=opt;
+        }
+      }
+      console.log("from ihm, optionElt.innerText:" + optionElt.innerText);
+      tauxSelectElt.value=optionElt.value;
+      fixture.detectChanges();
+
+     
+      console.log("from ihm, tauxSelectElt.value:" + tauxSelectElt.value);
+      tauxSelectElt.dispatchEvent(new Event('change'));
+
+      fixture.detectChanges();
+   
+      let spanTva = compNativeElt.querySelector('#spanTva');
+      console.log("from ihm, tva:" + spanTva.innerText);
+      expect(Number(spanTva.innerText)).toBe(20);
+     
+     
+      });
+
 });
 
 // ng test --include=**/tva.component.spec.ts
